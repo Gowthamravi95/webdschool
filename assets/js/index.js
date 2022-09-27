8406d418d1cb15175cb3bb49a3259d77463065f4
gowthamravi95 <gowthamhawks@gmail.com>
2022-09-27 11:38:11 +0530

    footer update

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,8 +7,9 @@
 $(document).ready(() => {
 
     // navlink collapse button 
-    $('.collapse-icon').click(()=>{
+    $('.collapse-icon').click(() => {
         $(' .navlinks').slideToggle();
+        $('.courses-drop-menu').hide();
     })
 
 
@@ -18,135 +19,117 @@ $(document).ready(() => {
         $('.announce-wrapper').hide();
     })
 
-    $('.course').mouseenter(function(){
-        $(this).children('button').css({"background-color": "white", "color": "#FF2359"})
-        $(this).children('button').children().css({"transform": "rotate(180deg)"})
+    $('.course').mouseenter(function () {
+        $(this).children('button').css({ "background-color": "white", "color": "#FF2359" })
+        $(this).children('button').children().css({ "transform": "rotate(180deg)" })
     })
-    $('.course ').mouseleave(function(){
-        $(this).children('button').css({"background-color": "#FF2359", "color": "white"})
-        $(this).children('button').children().css({"transform": "rotate(0deg)"})
+    $('.course ').mouseleave(function () {
+        $(this).children('button').css({ "background-color": "#FF2359", "color": "white" })
+        $(this).children('button').children().css({ "transform": "rotate(0deg)" })
     })
 
 
 
 
 
-$('.navlinks ul li ').click(function () {
+    $('.navlinks ul li ').click(function () {
 
-    $('.navlinks ul li a').removeClass("active");
-    $('.navlinks ul li i').removeClass("active");
-    $('.navlinks ul li svg').removeClass("active");
-    $('.navlinks ul li ').removeClass("active-li");
-    $(this).addClass("active-li");
-    $(this).children().addClass("active");
-    if ($(window).width() < 1200){
-        $('.navlinks').slideToggle();
-    }
+        $('.navlinks ul li a').removeClass("active");
+        $('.navlinks ul li i').removeClass("active");
+        $('.navlinks ul li svg').removeClass("active");
+        $('.navlinks ul li ').removeClass("active-li");
+        $(this).addClass("active-li");
+        $(this).children().addClass("active");
+        if ($(window).width() < 1200) {
+            $('.navlinks').slideToggle();
+        }
 
-})
+    })
 
-$('.courses-drop-menu .course-list .courses').hover(function(){
-    $(this).children().toggle()
+    $('.courses-drop-menu .course-list .courses').hover(function () {
+        $(this).children().toggle()
 
-})
-$('.course').hover(function(){
-       
-    $(this).children(".dropdown").toggle()
-})
+    })
+    $('.course').hover(function () {
 
-})
-
-
-        // console.log('working');
-
-
-
-
-        let svg=[
-            
-                {
-                    id:"1",
-                    svg:''
-                },
-                {
-                    id:"2",
-                },
-                {
-                    id:"3",
-                },
-                {
-                    id:"4",
-                },
-            ]
+        $(this).children(".dropdown").toggle()
+        $(' .navlinks').hide();
         
+    })
+    $('.courses-drop-menu .course-list  .online').click(function () {
+        $(this).children().toggle()
+        $(this).next().children().hide()
+       
+    })
+    $('.courses-drop-menu .course-list  .offline').click(function () {
+        $(this).children().toggle()
+        $(this).prev().children().hide()
+
+       
+    })
+    $('.course button').click(function () {
+
+        $(this).parent().children(".dropdown").toggle()
+        $(' .navlinks').hide();
+
+    })
 
 
-$(document).ready(() => {
-
-svg.map((e)=>{
-    console.log(e.id)
-})
 
 
+    var c, currentScrollTop = 0,
+        topbar = $('.top-bar');
 
-})
-
-
-
-
-$(document).ready(function () {
-  
-    
-     var c, currentScrollTop = 0,
-         topbar = $('.top-bar');
-  
-     $(window).scroll(function () {
+    $(window).scroll(function () {
         var a = $(window).scrollTop();
         var b = topbar.height();
 
         currentScrollTop = a;
-       
+
         if (c < currentScrollTop && a > b + b) {
-          topbar.addClass("hide")
+            topbar.addClass("hide")
         } else if (c > currentScrollTop && !(a <= b)) {
-          topbar.removeClass("hide");
+            topbar.removeClass("hide");
 
         }
         c = currentScrollTop;
 
-       
+
     });
-    
-  });
 
 
 
-  $(document).ready(() => {
-    function resizetopbarheight(){
-        var prevHeight=$('.top-bar').height();
-        $('.main').css({"marginTop":prevHeight + "px"});
-        
+
+    function resizetopbarheight() {
+        var prevHeight = $('.top-bar').height();
+        $('.main').css({ "marginTop": prevHeight + "px" });
+
     }
-   
+
     resizetopbarheight();
 
-   $(window).resize(()=>{
-    resizetopbarheight();
+    $(window).resize(() => {
+        resizetopbarheight();
 
-      
-   })
-   
-$('.companies').slick({
-    autoplay: true,
-     autoplaySpeed: 0,
-     speed: 20000,
-     cssEase: 'linear',
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  pauseOnFocus: false,
-  pauseOnHover: false
+
+    })
+
+    $('.companies').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 15000,
+        cssEase: 'linear',
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        rtl: false,
+        variableWidth: true,
+        adaptiveHeight: true,
+        infinte:true,
+    })
+    $('.slick-prev').text("");
+    $('.slick-next').text("");
+
+
+
+
 })
-$('.slick-prev').text("");
-$('.slick-next').text("");
-})
-    
