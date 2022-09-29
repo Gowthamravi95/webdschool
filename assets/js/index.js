ee96093efc2b18893879386063ec90a1dce42fbe
gowthamravi95 <gowthamhawks@gmail.com>
2022-09-29 19:20:55 +0530

    gors

--- assets/js/index.js
+++ assets/js/index.js
@@ -297,6 +297,46 @@ $(document).ready(() => {
 
 
 
+    var moretext = `Read More<i class="fa-solid fa-angle-down"></i>`;
+    var lesstext = `Read Less<i class="fa-solid fa-angle-up"></i>`;
+
+
+    $('.more').each(function () {
+
+            console.log($(this).height());
+
+            let h =$(this).height();
+
+            if(h==120){
+                $(this).after('<div><a href="" class="morelink">' + moretext + '</a></div>');
+
+            }else{
+                $(this).addClass('nobefore');
+            }
+
+        });
+
+        $(".morelink").click(function () {
+            if ($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                    $(this).html(moretext);
+
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+             
+            }
+            $(this).parent().prev().toggleClass('moreStyle nobefore');
+            return false;
+        });
+
+
+
+
+
+
+
+
 
 
 
